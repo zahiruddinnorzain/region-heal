88727c9bac3484f27f4b62f3e55e55ccd74ba4ee
zahiruddin <kepaknaga@gmail.com>
2021-10-10 01:07:39 +0800

    update bootstrap 2

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,7 +4,7 @@
   <title>Region Heal</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
